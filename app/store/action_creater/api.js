e9e5cf147f8ea13b0eaa2dd7e2445aab3f8d55fe
misael dessalegn <misaeldessalegn@gmail.com>
2024-02-13 01:38:21 +0300

    save all

--- app/store/action_creater/api.js
+++ app/store/action_creater/api.js
@@ -1,3 +1,3 @@
 import { createAction } from "@reduxjs/toolkit";
 export const apiCallBegan=createAction('api/callBegan')
-export const baseURL=process.env.NEXT_PUBLIC_API_URL
+export const baseURL=process.env.local.NEXT_PUBLIC_API_URL
